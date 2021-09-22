8f795c9fed6d88fdfaa989c1ee64aa41e4e44593
Jasmine Ryon <jryon12@priorypride.net>
2021-09-22 14:24:20 -0700

    got rid of instance variables in song controller

--- backend/app/controllers/songs_controller.rb
+++ backend/app/controllers/songs_controller.rb
@@ -1,48 +1,45 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :update, :destroy]
+  # before_action :set_song, only: [:show, :update, :destroy]
 
   def index
-    @songs = Song.all
-
-    render json: @songs
+    songs = Song.all
+    render json: songs
   end
 
   def show
-    render json: @song
+    song = Song.find(params[:id])
+    render json: song
   end
 
-  # POST /songs
   def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      render json: @song, status: :created, location: @song
+    song = Song.new(song_params)
+    if song.save
+      render json: song, status: :created, location: song
     else
-      render json: @song.errors, status: :unprocessable_entity
+      render json: song.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /songs/1
   def update
-    if @song.update(song_params)
-      render json: @song
+    song = Song.find(params[:id])
+    if song.update(song_params)
+      render json: song
     else
-      render json: @song.errors, status: :unprocessable_entity
+      render json: song.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /songs/1
   def destroy
-    @song.destroy
+    song = Song.find(params[:id])
+    song.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_song
-      @song = Song.find(params[:id])
-    end
+  
+    # def set_song
+    #   song = Song.find(params[:id])
+    # end
 
-    # Only allow a list of trusted parameters through.
     def song_params
       params.require(:song).permit(:title, :artist, :image, :chords, :genre_id)
     end
