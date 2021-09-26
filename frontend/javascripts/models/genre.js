f811c073893f4ce2c6af7d61611dee9ad5d7c732
Jasmine Ryon <jryon12@priorypride.net>
2021-09-25 17:10:48 -0700

    retrieveAll genres

--- frontend/javascripts/models/genre.js
+++ frontend/javascripts/models/genre.js
@@ -2,9 +2,14 @@ class Genre {
     static all = []
 
     constructor({name, id, songs = []}){
-        this.name = name,
-        this.id = id,
+        this.name = name
+        this.id = id
         this.songs = songs
         Genre.all.push(this)
+    }
+
+    // class method
+    static retrieveAll {
+        return this.all
     }
 }
