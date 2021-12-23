a19b0a9910e1c881b8e85d541956c14917b57bfe
Jasmine Ryon <jryon12@priorypride.net>
2021-12-23 14:41:57 -0800

    changed cors file

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://silly-davinci-6acede.netlify.app'
 
     resource '*',
       headers: :any,
